2750078c7e5f06178b11da7c021255dc0519197e
Pritesh Kumar <160621539+pritesh45@users.noreply.github.com>
2025-02-12 13:57:08 +0530

    Time: 8 ms (87.17%), Space: 47 MB (15.96%) - LeetHub

---
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int[][] merge(int[][] intervals) {
+        //sorting based on start time
+        Arrays.sort(intervals,(a,b)->a[0]-b[0]);
+        //pointer for the position in merged array
+        int index=0;
+        //iterate through intervals and merge
+        for(int i=1;i<intervals.length;i++){
+            if(intervals[index][1]>=intervals[i][0]){
+                //merge intervals
+                intervals[index][1]=Math.max(intervals[index][1],intervals[i][1]);
+            }else{
+                //move to next interval
+                index++;
+                intervals[index]=intervals[i];
+            }
+        }
+        return Arrays.copyOf(intervals,index+1);
+    }
+}
