1194ea93ae1697a448d9927ef787337a18755f51
Pritesh Kumar <160621539+pritesh45@users.noreply.github.com>
2025-03-12 18:18:54 +0530

    Time: 0 ms (100%), Space: 41.1 MB (5.24%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,21 +1,21 @@
 class Solution {
-    private int helper(int n,int[] dp){
-        if(n==0||n==1){
+    static int find(int[]dp,int n,int ind){
+        if(ind==n){
             return 1;
         }
-        if(dp[n]!= -1){
-            return dp[n];
+        if(ind>n){
+            return 0;
         }
-        dp[n]=helper(n-1,dp)+helper(n-2,dp);
-        return dp[n];
+        if(dp[ind]!=-1)
+        return dp[ind];
+        int left=find(dp,n,ind+1);
+        int right=find(dp,n,ind+2);
+        return  dp[ind]=left+right;
     }
     public int climbStairs(int n) {
-    
         int[] dp=new int[n+1];
         Arrays.fill(dp,-1);
-        
-     
-        return helper(n,dp);
+        return find(dp,n,0);
         
     }
 }
