257ea36bceee0c4785064bda4e3258141f287667
Pritesh Kumar <160621539+pritesh45@users.noreply.github.com>
2025-07-22 01:57:59 +0530

    Time: 14 ms (100%), Space: 45.6 MB (78.86%) - LeetHub

---
+++ 1302-delete-characters-to-make-fancy-string/1302-delete-characters-to-make-fancy-string.java
@@ -0,0 +1,19 @@
+class Solution {
+    public String makeFancyString(String s) {
+        //two pointer technique
+        char[] chars=s.toCharArray();
+        char last=chars[0];
+        int count=1;
+        int position=1;
+
+        for(int i=1;i<chars.length;i++){
+            if(chars[i]!=last){
+                last=chars[i];
+                count=0;
+            }
+            if(++count >2)continue;
+            chars[position++]=chars[i];
+        }
+        return new String(chars,0,position);
+    }
+}
